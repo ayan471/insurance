f96b413be3ae1ece1ba72346d3392d7d9475fc05
ayan471 <pakhiraayan@gmail.com>
2025-05-08 19:16:44 +0530

    int

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-import AboutSection from "@/components/about-section";
-import AppPromoSection from "@/components/app-promo-section";
-import Faq from "@/components/faq";
-import Hero from "@/components/hero";
-import PartnersCarousel from "@/components/partners-section-alt";
-import Services from "@/components/services";
-import Statistics from "@/components/statistics";
-import Testimonials from "@/components/testimonials";
-import WhyChooseUs from "@/components/why-choose-us";
+// import AboutSection from "@/components/about-section";
+// import AppPromoSection from "@/components/app-promo-section";
+// import Faq from "@/components/faq";
+// import Hero from "@/components/hero";
+// import PartnersCarousel from "@/components/partners-section-alt";
+// import Services from "@/components/services";
+// import Statistics from "@/components/statistics";
+// import Testimonials from "@/components/testimonials";
+// import WhyChooseUs from "@/components/why-choose-us";
 
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <Hero />
-      <AboutSection />
-      <Services />
+// export default function Home() {
+//   return (
+//     <main className="min-h-screen bg-background">
+//       <Hero />
+//       <AboutSection />
+//       <Services />
 
-      <Statistics />
-      <WhyChooseUs />
-      <PartnersCarousel />
-      <Faq />
+//       <Statistics />
+//       <WhyChooseUs />
+//       <PartnersCarousel />
+//       <Faq />
 
-      <Testimonials />
-      <AppPromoSection />
-    </main>
-  );
-}
+//       <Testimonials />
+//       <AppPromoSection />
+//     </main>
+//   );
+// }
